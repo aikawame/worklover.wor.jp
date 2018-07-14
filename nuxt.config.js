5d88c2ae0e5afd6681da285267cf05794ce774ff
Masahiro Aikawa <aikawame@wor.jp>
2018-07-14 16:12:33 +0900

    ライブラリーと設定を追加する

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,11 +3,22 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'worklover.wor.jp',
+    title: 'ワクラバ Worklover',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+      { hid: 'description', name: 'description', content: 'ワクラバでは、中の人が制作したサービスやアプリケーションを紹介しています。' },
+      { hid: 'keyword', name: 'keyword', content: 'ワクラバ,WorkLover,aikawame' },
+      { property: 'og:type', content: 'website' },
+      { property: 'og:site_name', content: 'ワクラバ' },
+      { property: 'og:title', content: 'ワクラバ WorkLover' },
+      { property: 'og:url', content: 'https://worklover.wor.jp/' },
+      { property: 'og:image', content: 'https://worklover.wor.jp/img/ogp.png' },
+      { property: 'og:image:width', content: '1200' },
+      { property: 'og:image:height', content: '630' },
+      { property: 'og:description', content: 'ワクラバでは、中の人が制作したサービスやアプリケーションを紹介しています。' },
+      { name: 'twitter:card', content: 'summary_large_image' },
+      { name: 'twitter:site', content: '@aikawame' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
@@ -34,5 +45,9 @@ module.exports = {
         })
       }
     }
-  }
-}
+  },
+  css: [
+    '~/assets/css/default.scss'
+  ]
+};
+
